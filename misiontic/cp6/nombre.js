d6ba4ce6047b8159f1057c181536784faf49ed53
williambenavides <wmurcia@uninorte.edu.co>
2022-09-18 21:17:08 -0500

    actualizacion sesion 7

--- misiontic/cp6/nombre.js
+++ misiontic/cp6/nombre.js
@@ -1,26 +1,35 @@
 function validar_formulario() {
-    var username = document.formRegistro.username;
-    var email = document.formRegistro.correo;
+    let username = document.formRegistro.username;
+    let email = document.formRegistro.correo;
     var password = document.formRegistro.password;
   
-    var username_len = username.value.length;
+    let username_len = username.value.length;
     if (username_len == 0 || username_len < 8) {
       //alert("Debes ingresar un username con min. 8 caracteres");
       passid.focus();
       return false;
     }
   
-    var formato_email = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+    let formato_email = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
     if (!email.value.match(formato_email)) {
       //alert("Debes ingresar un email electronico valido!");
       email.focus();
       return false;
     }
   
-    var passid_len = password.value.length;
+    let passid_len = password.value.length;
     if (passid_len == 0 || passid_len < 8) {
       //alert("Debes ingresar una password con mas de 8 caracteres");
       passid.focus();
     }
   }
+
+function mostrarPassword() {
+  let obj = document.getElementById("password");
+  obj.type = "text";
+}
+function ocultarPassword() {
+  let obj = document.getElementById("password");
+  obj.type = "password";
+}
 
